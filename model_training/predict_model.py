a5ede7eb74be7035d6c213c708187754fa34934b
DorianTsai <dorian.tsai@gmail.com>
2023-06-05 22:19:23 +1000

    minor updates

--- model_training/predict_model.py
+++ model_training/predict_model.py
@@ -9,17 +9,33 @@ Predict using trained model
 from ultralytics import YOLO
 import os
 import glob
+import cv2 as cv
+import torch
 
 # load model
 print('loading model')
-model_path = '/home/zachary/UVI_Training/code/uvi_corals/model_training/runs/segment/train5/weights/last.pt'
+model_path = '/home/zachary/UVI_Training/code/uvi_corals/model_training/runs/segment/train9/weights/last.pt'
 model = YOLO(model_path)
+
+
+device = torch.device('cuda')
 
 print('predicting on images')
 img_dir = '/home/zachary/UVI_Training/data/yolo_seg/train/images'
 img_list = sorted(glob.glob(os.path.join(img_dir, '*.jpg')))
 img_list = img_list[0:100]
-results = model(img_list, save=True, save_txt=True, save_conf=True, boxes=True)
+results = model(img_list, 
+                save=True, 
+                save_txt=True, 
+                save_conf=True, 
+                boxes=True,
+                conf=0.1, # intentionally very low for debugging purposes
+                agnostic_nms=True,
+                device=device)
+
+# res = model(img)
+# res_plotted = res[0].plot()
+# cv.imshow("result", res_plotted)
 
 # for interactive debugger in terminal:
 import code
--- model_training/train_model.py
+++ model_training/train_model.py
@@ -14,12 +14,12 @@ import os
 import glob as glob
 
 # load pre-trained model
-model = YOLO('yolov8n-seg.pt')
+model = YOLO('yolov8m-seg.pt')
 
 # train model
 print('Model Training:')
 data_file = '/home/zachary/UVI_Training/code/uvi_corals/model_training/uvi_training.yaml'
-model.train(data=data_file, epochs=100, imgsz=640)
+model.train(data=data_file, epochs=200, imgsz=640, batch=4)
 
 print('Model Validation:')
 metrics = model.val()
@@ -27,7 +27,7 @@ metrics = model.val()
 print('Model Inference:')
 image_file = '/home/zachary/UVI_Training/data/images/TCRMP20180228_clip_SRD/TCRMP20180228_clip_SRD_T101.jpg'
 results = model(image_file)
-
+print(results)
 
 
 
