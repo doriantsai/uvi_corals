2e6d1e05721792c7bb5c054b565e311fd4892658
Dorian Tsai <dorian.tsai@gmail.com>
2023-06-07 19:10:26 +1000

    train model for bounding boxes

--- model_training/train_model.py
+++ model_training/train_model.py
@@ -14,18 +14,18 @@ import os
 import glob as glob
 
 # load pre-trained model
-model = YOLO('yolov8m-seg.pt')
+model = YOLO('yolov8x.pt')
 
 # train model
 print('Model Training:')
-data_file = '/home/zachary/UVI_Training/code/uvi_corals/model_training/uvi_training_combined_2.yaml'
-model.train(data=data_file, epochs=100, imgsz=640, batch=5)
+data_file = '/home/dorian/Code/uvi_corals/model_training/uvi_training_combined_2.yaml'
+model.train(data=data_file, epochs=100, imgsz=640, batch=16)
 
 print('Model Validation:')
 metrics = model.val()
 
 print('Model Inference:')
-image_file = '/home/zachary/UVI_Training/data/images/TCRMP20180228_clip_SRD/TCRMP20180228_clip_SRD_T101.jpg'
+image_file = '/home/dorian/Data/uvi/images/TCRMP20180228_clip_SRD/TCRMP20180228_clip_SRD_T101.jpg'
 results = model(image_file)
 print(results)
 
